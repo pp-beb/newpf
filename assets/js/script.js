af26bbf2c21895fc1a19df85e5b021207ada7b05
Prithak koirala <prithakkoirala44@gmail.com>
2025-05-16 09:47:19 +0545

    updating new files

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+	        window.onload = function() { document.body.classList.remove('is-preload'); }
+			window.ontouchmove = function() { return false; }
+			window.onorientationchange = function() { document.body.scrollTop = 0; }
+
+            function uncompleted(){
+                alert("Sorry!! The project is not yet completed.");
+            }
